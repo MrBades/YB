9d0bf387187dae63398bf663e48986483ec803e7
Bades <39216599+MrBades@users.noreply.github.com>
2026-05-29 12:17:43 +0100

    Update settings.py

--- YB/django_backend/yeedem_project/settings.py
+++ YB/django_backend/yeedem_project/settings.py
@@ -76,23 +76,35 @@ WSGI_APPLICATION = 'yeedem_project.wsgi.application'
 # Falls back to local sqlite3 for easier local development
 DATABASE_URL = os.environ.get('DATABASE_URL') or os.environ.get('POSTGRES_URL')
 
-if DATABASE_URL:
+if os.environ.get('POSTGRES_URL'):
     DATABASES = {
         'default': dj_database_url.config(
-            default=DATABASE_URL,
+            default=os.environ.get('POSTGRES_URL'),
             conn_max_age=600,
-            conn_health_checks=True,
             ssl_require=True
         )
     }
-else:
+elif os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+elif 'collectstatic' in sys.argv:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.dummy',
         }
     }
-
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
